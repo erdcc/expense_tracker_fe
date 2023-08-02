18d01a0ce98cda752dc7d6434507e03e98fa8e4e
erd <erdincc97@hotmail.com>
2023-08-02 14:48:39 +0200

    port changed

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export default () => {
     const token = localStorage.getItem("token")
     return axios.create({
-        baseURL: "http://173.82.58.139:80",
+        baseURL: "http://173.82.58.139:443",
         headers: {
             Authorization: token
         }
