5d80c680ffddddff91f4f57e97322b9b796c6d9f
erd <erdincc97@hotmail.com>
2023-08-03 21:42:41 +0200

    port changed

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export default () => {
     const token = localStorage.getItem("token")
     return axios.create({
-        baseURL: "http://173.82.58.139:80",
+        baseURL: "https://tracker.toolgen.dev:5000",
         headers: {
             Authorization: token
         }
