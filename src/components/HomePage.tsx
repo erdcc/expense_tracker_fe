1b8d8660a10f8bbf7f315a020f55c3f6a90bfabf
erd <erdincc97@hotmail.com>
2023-08-06 01:20:39 +0200

    datepicker added

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,20 @@
 import { Pie } from "@ant-design/plots";
 import { useDispatch, useSelector } from "react-redux";
 import { AppState } from "../store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getRecords } from "../store/actions/recordAction";
+import { DatePicker, DatePickerProps } from "antd";
 
 const HomePage = () => {
+  const { RangePicker } = DatePicker;
   const { data: recordData, loading } = useSelector((state: AppState) => state.records)
   const dispatch = useDispatch<any>()
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth()
+
+  //data
+  const [selectedDate,setSelectedDate]=useState(new Date())
+  const onChangeDate: DatePickerProps['onChange'] = (date, dateString) => {
+    setSelectedDate(date!.toDate())
+  };
   const dataExpense = recordData.filter(record => record.category.type === "expense").map(record => {
     const type = record.title
     const value = record.amount
@@ -17,7 +23,7 @@ const HomePage = () => {
   const dataIncome = recordData.filter(record => record.category.type === "income")
     .filter(record => {
       const recordMonth = new Date(record.createdAt).getMonth()
-      return recordMonth === currentMonth;
+      return recordMonth === selectedDate.getMonth();
     })
     .map(record => {
       const type = record.title
@@ -25,6 +31,8 @@ const HomePage = () => {
       return ({ type, value })
     })
   useEffect(() => { dispatch(getRecords()) }, [dispatch])
+
+  //token
   const token = localStorage.getItem("token");
   useEffect(() => {
     if (token) {
@@ -38,6 +46,8 @@ const HomePage = () => {
 
   const sumIncome = dataIncome.reduce((total, item) => total + item.value, 0);
   const sumExpense = dataExpense.reduce((total, item) => total + item.value, 0);
+
+  
 
   const config = {
     appendPadding: 10,
@@ -56,12 +66,8 @@ const HomePage = () => {
       },
     },
     interactions: [
-      {
-        type: 'element-selected',
-      },
-      {
-        type: 'element-active',
-      },
+      {type: 'element-selected',},
+      {type: 'element-active',},
     ],
     statistic: {
       title: {
@@ -89,23 +95,31 @@ const HomePage = () => {
   };
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: "wrap" }}>
-      <div style={{ textAlign: "center", width: "100%", fontSize: 36, fontWeight:"bold" ,color:"#001aff"}}>{currentDate.toLocaleString('default', { month: 'long' }).toUpperCase()}</div>
-      <div style={{ flex: '0 0 50%', maxWidth: '50%', padding: '10px' }}>
+
+      <div style={{ flex: '0 0 50%', maxWidth: '40%', paddingTop: '10px' }}>
         <Pie {...config}
           data={token && dataExpense.length ? dataExpense : [{ type: "", value: 0 }]}
           statistic={{
             ...config.statistic,
             title: { ...config.statistic.title, content: "EXPENSE" },
             content: {
-              ...config.statistic.content, content: `${Intl.NumberFormat("tr-TR", { style: "currency", currency: "TRY" }).format(sumExpense)}`
-              , style: { ...config.statistic.content.style, color: "red" }
+              ...config.statistic.content, content: `${Intl.NumberFormat("tr-TR", { style: "currency", currency: "TRY" }).format(sumExpense)}`,
+              style: { ...config.statistic.content.style, color: "red" }
             }
           }}
           loading={loading}
           legend={false}
         />
       </div>
-      <div style={{ flex: '0 0 50%', maxWidth: '50%', padding: '10px' }}>
+
+      <div style={{ display: "flex", flexDirection: "column", maxWidth: '20%', textAlign: "center", paddingTop: "10px", justifyContent:"center" }}>
+        <p style={{ fontSize: 36, fontWeight: "bold", color: "#001aff" }}>{selectedDate.toLocaleString('default', { month: 'long' }).toUpperCase()}</p>
+        <DatePicker onChange={onChangeDate} picker="month" size="large" format={"MM/YYYY"} />
+        <p style={{paddingTop:"10px"}}> OR </p>
+        <RangePicker size="large" format={"DD/MM/YYYY"}/>
+      </div>
+
+      <div style={{ flex: '0 0 50%', maxWidth: '40%', paddingTop: '10px' }}>
         <Pie
           {...config}
           data={token && dataIncome.length ? dataIncome : [{ type: "", value: 0 }]}
@@ -113,8 +127,8 @@ const HomePage = () => {
             ...config.statistic,
             title: { ...config.statistic.title, content: "INCOME" },
             content: {
-              ...config.statistic.content, content: `${Intl.NumberFormat("tr-TR", { style: "currency", currency: "TRY" }).format(sumIncome)}`
-              , style: { ...config.statistic.content.style, color: "green" }
+              ...config.statistic.content, content: `${Intl.NumberFormat("tr-TR", { style: "currency", currency: "TRY" }).format(sumIncome)}`,
+              style: { ...config.statistic.content.style, color: "green" }
             }
           }}
           loading={loading}
