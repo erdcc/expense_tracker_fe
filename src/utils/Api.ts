014e1ef269d45f152454620be0c231fe28d98933
erd <erdincc97@hotmail.com>
2024-02-10 18:45:13 +0300

    api change

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export default () => {
     const token = localStorage.getItem("token")
     return axios.create({
-        baseURL: "https://tracker.toolgen.dev",
+        baseURL: "https://api-tracker.toolgen.dev",
         headers: {
             Authorization: token
         }
